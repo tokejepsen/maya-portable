5bf268d3b1f2cf67161cd0b5775a80ac0acc9080
Toke Jepsen <tokejepsen@gmail.com>
2019-04-05 08:02:43 +0100

    Update maya_2016.bat

--- Maya/maya_2016.bat
+++ Maya/maya_2016.bat
@@ -4,5 +4,6 @@ set PYTHONPATH=%PYTHONPATH%;%CD%\python;%CD%\python\Tapp\System
 set MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;%CD%\scripts
 set XBMLANGPATH=%XBMLANGPATH%;%CD%\icons
 set MAYA_PRESET_PATH=%MAYA_PRESET_PATH%;%CD%\presets
+set MAYA_MODULE_PATH=%MAYA_MODULE_PATH%;%CD%\modules
 cd "%~p1"
 start "" "C:/Program Files/Autodesk/Maya2016/bin/maya.exe" %*
