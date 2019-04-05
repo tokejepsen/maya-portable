4dbcbf538d8966d48374111eea4a23c8db91d33c
Toke Stuart Jepsen <tokejepsen@gmail.com>
2019-04-05 08:19:24 +0100

    Fix all launchers for pythonpath

--- maya_2019.bat
+++ maya_2019.bat
@@ -1,6 +1,6 @@
 set MAYA_SHELF_PATH=%MAYA_SHELF_PATH%;%CD%\shelves
 set MAYA_PLUG_IN_PATH=%MAYA_PLUG_IN_PATH%;%CD%\plugins\2016
-set PYTHONPATH=%PYTHONPATH%;%CD%\python;%CD%\python\Tapp\System
+set PYTHONPATH=%PYTHONPATH%;%CD%\pythonpath;%CD%\python\Tapp\System
 set MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;%CD%\scripts
 set XBMLANGPATH=%XBMLANGPATH%;%CD%\icons
 set MAYA_PRESET_PATH=%MAYA_PRESET_PATH%;%CD%\presets
